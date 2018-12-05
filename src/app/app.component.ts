e614449a1489c0296845393673c0cdf3dbe8d898
Stan <buht.95@gmail.com>
2018-12-06 02:29:20 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MessagesService, SpinnerService } from './core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+    constructor(
+        public spinnerService: SpinnerService,
+        public messagesService: MessagesService,
+        private router: Router) { }
+
+    onActivate($event) {
+        console.log('Activated Component', $event);
+    }
+
+    onDeactivate($event) {
+        console.log('Deactivated Component', $event);
+    }
+
+    onDisplayMessages(): void {
+        this.router.navigate([{ outlets: { messages: ['messages'] } }]);
+        this.messagesService.isDisplayed = true;
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { LayoutModule } from './layout/layout.module';
+import { AppRoutingModule } from './app-routing.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CoreModule } from './core/core.module';
+// import { UsersModule } from './users/users.module';
+// import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        LayoutModule,
+        CoreModule,
+        SharedModule,
+        // AdminModule,
+        TasksModule,
+        // UsersModule,
+        AppRoutingModule
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+    constructor(router: Router) {
+        console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+    }
+}
